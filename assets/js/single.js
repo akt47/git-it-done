04bbf4d6ce0315df8dabec7c24853ec288db59c1
AK47 <kunle89@gmail.com>
2021-04-05 15:01:15 -0500

    api functions

--- assets/js/single.js
+++ assets/js/single.js
@@ -41,8 +41,8 @@ var getRepoIssues = function(repo) {
       });
     }
     else {
-      console.log(response);
-      alert("There was a problem with your request!");
+      //if not successful, redirect to homepage
+      document.location.replace("./index.html");
     }
   });
 };
